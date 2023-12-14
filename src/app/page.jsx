966dcdf758681641ff2d6a07ecd140abc072147d
maximedoaw <maximedoaw204@gmail.com>
2023-12-14 10:41:55 -0800

    Install next.js for IMDB clone

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+//import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div>
+    <title>IMDB clone</title>
+    <meta content="this is imdb clone" name='description' />
+    <h1>Home</h1>
+    </div>
+
+  )
+}
