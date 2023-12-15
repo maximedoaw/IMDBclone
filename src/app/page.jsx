b569e929e59c4c4a54ada7b6c7fcddb2c76a8628
maximedoaw <maximedoaw204@gmail.com>
2023-12-15 03:08:38 -0800

    Add header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,9 +4,7 @@ export default function Home() {
     <div>
     <title>IMDB clone</title>
     <meta content="this is imdb clone" name='description' />
-    <div class="text-red-400">
-      Hello world
-    </div>
+    <div className='text-red-500 text-2xl sm:text-2xl'>Home</div>
    </div>
 
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import MenuItem from './MenuItem'
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import Link from 'next/link'
+function Header() {
+  return (
+    <div className='flex justify-between mx-2
+    max-w-6xl sm:mx-auto items-center py-6'>
+    
+        <div className="flex lg:flex">
+            <MenuItem title="HOME" address="/" Icon={AiFillHome}/>
+            <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill}/>
+        </div>
+        <div className="">
+          <Link href="/">
+            <h2 classname='text-4xl'>
+              <span className='font-bold bg-amber-500
+              py-2 px-1 rounded-lg mr-1
+              '>IMDB</span>
+              <span className='text-xl hidden
+              sm:inline'>Clone</span>
+            </h2>
+          </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Header
