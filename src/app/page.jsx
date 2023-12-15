405c3e2c3131fd368f19ffde006012ec6bcbf6c5
maximedoaw <maximedoaw204@gmail.com>
2023-12-14 19:57:59 -0800

    install tailwind css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,13 @@
-import Image from 'next/image'
-//import styles from './page.module.css'
-
+import './globals.css'
 export default function Home() {
   return (
     <div>
     <title>IMDB clone</title>
     <meta content="this is imdb clone" name='description' />
-    <h1>Home</h1>
+    <div class="text-red-400">
+      Hello world
     </div>
+   </div>
 
   )
 }
