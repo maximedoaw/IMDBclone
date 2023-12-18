ee289eea66e8e2236f9da336af3691c982ee5cf0
maximedoaw <maximedoaw204@gmail.com>
2023-12-18 02:25:53 -0800

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae unde mollitia atque placeat, soluta minus possimus accusamus nisi nam dolores rem est enim sunt quo iure, et provident ipsam ad.
+      Ut dolores odio consectetur tempora ex recusandae aperiam ullam. Laborum quod eligendi sunt nihil voluptate aut odio ipsam ipsa alias, fuga velit, iste recusandae? Pariatur unde officiis voluptatem possimus ad.
+      Quaerat quasi cupiditate natus atque expedita veniam tempore minima, repudiandae ex assumenda ab alias doloremque dignissimos obcaecati voluptates! Voluptas esse itaque, magnam dicta consequatur saepe vitae laborum. Saepe, dolore ullam?
+      Praesentium odio dignissimos nisi tempore culpa. In odit ipsa et. Blanditiis quisquam ratione quos provident quo, incidunt reiciendis dolorem tempora laboriosam facilis! Obcaecati maiores fugiat temporibus consectetur reiciendis doloribus repellat!
+      Doloremque iste, officia inventore minus, dolore fuga expedita explicabo natus quibusdam veritatis veniam in tenetur cumque reiciendis deserunt architecto, impedit illum ipsum sapiente error. Ipsum nulla earum minus ex! Aliquid?
+      Vero veritatis itaque animi qui ratione, ipsa perferendis quam et impedit atque aut accusantium nam! Hic aliquam delectus labore, possimus dolorum sequi? Autem atque maxime, minima perspiciatis ad quia iusto!</p>
+      <p> Praesentium laboriosam dignissimos omnis doloremque veritatis, velit voluptatibus voluptates reprehenderit ab eveniet quod tenetur provident id dolorum ducimus quos eum ipsam possimus aperiam cum porro nobis culpa perspiciatis laborum? Blanditiis?
+      Rem accusantium, nemo eius eligendi voluptatibus nostrum molestiae, ratione doloribus sequi necessitatibus dolorum voluptates voluptatum magnam. Impedit iusto repellat, inventore provident aliquid officiis neque nesciunt. Ratione officia beatae incidunt laboriosam.
+      Vero rerum, pariatur unde deserunt provident incidunt cum error quis quisquam autem delectus ipsam non libero atque iusto! Non culpa velit cupiditate repellat minus minima ipsam suscipit eos magni dolorum?
+      Inventore maxime error rerum nesciunt iste omnis quia asperiores quis animi impedit ut at exercitationem corrupti voluptatum sed, facere expedita dolore. Accusamus, nemo necessitatibus. Facere minima expedita at libero illum?
+      </p>
+    </div>
+  )
+}
+
+export default About
